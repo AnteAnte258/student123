8c692f8ecac15677499ee364e2d9ef5f0d8c3ebf
AnteAnte258 <anteante258@gmail.com>
2026-03-23 18:50:19 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp01
+{
+    internal class NewClass
+    {
+        //konflikt 612349123490127u48902389758723805780
+    }
+}
